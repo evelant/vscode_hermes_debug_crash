cff935714fd0dd22ca6f1b257e2a5c2cab0af34c
imagio <imagio@mailfence.com>
2022-09-02 12:43:23 -0400

    example of hermes debug failure

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
+export function test() {
+  console.log("Test");
+  console.log("Test2");
+}
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,8 +17,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
